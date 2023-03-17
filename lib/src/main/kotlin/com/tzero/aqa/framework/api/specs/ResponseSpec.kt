c6558077e631514adab4f836e9f87bff2f130c1a
Dmitriy Malohlovets <dmalohlovets@tzero.com>
2023-03-17 13:22:12 +0000

    Init

---
+++ lib/src/main/kotlin/com/tzero/aqa/framework/api/specs/ResponseSpec.kt
@@ -0,0 +1,13 @@
+package com.tzero.aqa.framework.api.specs
+
+import io.restassured.builder.ResponseSpecBuilder
+
+object ResponseSpec {
+    val `200` = ResponseSpecBuilder()
+        .expectStatusCode(200)
+        .build()!!
+
+    val `201` = ResponseSpecBuilder()
+        .expectStatusCode(201)
+        .build()!!
+}
