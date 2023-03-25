539c6f4eac731464b77f795ea74352ae6fa1f68f
Dmitriy Malohlovets <dmalohlovets@tzero.com>
2023-03-25 16:36:37 +0200

    spec utils

--- lib/src/main/kotlin/io/tzero/aqa/framework/api/specs/RequestSpec.kt
+++ lib/src/main/kotlin/io/tzero/aqa/framework/api/specs/RequestSpec.kt
@@ -6,19 +6,21 @@ import io.restassured.response.ExtractableResponse
 import io.restassured.response.Response
 
 object RequestSpec {
-    val json = RequestSpecBuilder()
-        .setContentType(ContentType.JSON)
-        .build()!!
+    val json =
+        RequestSpecBuilder()
+            .setContentType(ContentType.JSON)
+            .build()!!
 
-    fun base(xrftoken: String, tzmCookie: String) = RequestSpecBuilder()
-        .setContentType(ContentType.JSON)
-        .addHeaders(
-            mapOf(
-                "X-TZM-XSRF-TOKEN" to xrftoken,
-                "Cookie" to "tzm-access=$tzmCookie",
+    fun base(xrftoken: String, tzmCookie: String) =
+        RequestSpecBuilder()
+            .setContentType(ContentType.JSON)
+            .addHeaders(
+                mapOf(
+                    "X-TZM-XSRF-TOKEN" to xrftoken,
+                    "Cookie" to "tzm-access=$tzmCookie",
+                )
             )
-        )
-        .build()
+            .build()
 
     context (ExtractableResponse<Response>)
     fun base() = base(cookie("TZM-XSRF-TOKEN"), cookie("tzm-access"))
---
+++ lib/src/main/kotlin/io/tzero/aqa/framework/utils/SpecUtils.kt
@@ -0,0 +1,14 @@
+package io.tzero.aqa.framework.utils
+
+import io.restassured.module.kotlin.extensions.Extract
+import io.restassured.response.ExtractableResponse
+import io.restassured.response.Response
+import io.restassured.response.ValidatableResponse
+import io.tzero.aqa.framework.api.specs.RequestSpec
+
+object SpecUtils {
+    fun ValidatableResponse.toSpec() = Extract { RequestSpec.base() }
+
+    context (ExtractableResponse<Response>)
+    fun String.toJsonValue() = jsonPath().getString(this)
+}
