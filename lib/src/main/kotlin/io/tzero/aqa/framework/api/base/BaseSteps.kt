242d92327d0f4933e8d0d032ce265f8616d56436
Dmitriy Malohlovets <dmalohlovets@tzero.com>
2023-03-25 15:21:20 +0200

    added BaseSteps

---
+++ lib/src/main/kotlin/io/tzero/aqa/framework/api/base/BaseSteps.kt
@@ -0,0 +1,8 @@
+package io.tzero.aqa.framework.api.base
+
+import org.junit.platform.commons.logging.Logger
+import org.junit.platform.commons.logging.LoggerFactory
+
+class BaseSteps {
+    val log: Logger = LoggerFactory.getLogger(javaClass)
+}
