d415eb351d7da8fcf94c31de76332649565e599e
Dmitriy Malohlovets <dmalohlovets@tzero.com>
2023-03-31 12:30:01 +0300

    refactored

--- lib/src/main/kotlin/io/tzero/aqa/framework/utils/ArraysUtils.kt
+++ lib/src/main/kotlin/io/tzero/aqa/framework/utils/ArraysUtils.kt
@@ -4,9 +4,11 @@ import org.junit.jupiter.params.provider.Arguments
 
 object ArraysUtils {
     fun <T> Array<T>.toArgumentsStream() = map {
-        when (it) {
-            is Pair<*, *> -> Arguments.of(it.first, it.second)
-            else -> Arguments.of(it)
-        }
+        Arguments.of(
+            when (it) {
+                is Pair<*, *> -> arrayOf(it.first, it.second)
+                else -> it.toString()
+            }
+        )
     }.stream()
 }
