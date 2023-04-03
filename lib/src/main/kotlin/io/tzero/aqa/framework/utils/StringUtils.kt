73961206e8a09b19023357ce6847c626ebf3ec8e
Dmitriy Malohlovets <dmalohlovets@tzero.com>
2023-04-03 18:07:19 +0300

    +wrap

--- lib/src/main/kotlin/io/tzero/aqa/framework/utils/StringUtils.kt
+++ lib/src/main/kotlin/io/tzero/aqa/framework/utils/StringUtils.kt
@@ -5,4 +5,6 @@ import org.apache.commons.lang3.RandomStringUtils
 object StringUtils {
     fun generateRandomEmail(numberOfChars: Int = 16) =
         RandomStringUtils.randomAlphanumeric(numberOfChars) + "@tzero.com"
+
+    fun Any.wrap() = toString().let { "\"$it\"" }
 }
