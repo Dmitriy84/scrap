72c91f35f03b596c1f543dc37031ccf713fa8c8e
Dmitriy Malohlovets <dmalohlovets@tzero.com>
2023-04-04 16:34:58 +0300

    +SpaceAndNonPrintedSource

--- lib/src/main/kotlin/io/tzero/aqa/framework/sources/SpaceAndNonPrintedSource.kt
+++ lib/src/main/kotlin/io/tzero/aqa/framework/sources/SpaceAndNonPrintedSource.kt
@@ -2,9 +2,10 @@ package io.tzero.aqa.framework.sources
 
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.ArgumentsProvider
+import java.util.stream.Stream
 
 inline fun <reified T : Any> arguments(vararg args: T) = ArgumentsProvider {
-    args.map { Arguments.of(it) }.stream()
+    Stream.of(args).map { Arguments.of(it) }
 }
 
 class SpaceAndNonPrintedSource : ArgumentsProvider by arguments(arrayOf(" ", "\t", "\n"))
