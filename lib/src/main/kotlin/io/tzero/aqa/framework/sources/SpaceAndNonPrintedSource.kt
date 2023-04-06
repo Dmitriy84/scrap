095fa8e32d3e9d74f8a54167aea610db5b659c02
Dmitriy Malohlovets <dmalohlovets@tzero.com>
2023-04-06 16:26:09 +0300

    +SpaceAndNonPrintedSource.adapt refactored

--- lib/src/main/kotlin/io/tzero/aqa/framework/sources/SpaceAndNonPrintedSource.kt
+++ lib/src/main/kotlin/io/tzero/aqa/framework/sources/SpaceAndNonPrintedSource.kt
@@ -8,7 +8,7 @@ inline fun <reified T : Any> arguments(vararg args: T) = ArgumentsProvider {
     Stream.of(*args).map { Arguments.of(it) }
 }
 
-class SpaceAndNonPrintedSource : ArgumentsProvider by arguments(" ", "\t", "\n") {
-    fun adapt(vararg addition: Any) = provideArguments(null)
-        .map { Arguments.of(it.get()[0], *addition) }
-}
+class SpaceAndNonPrintedSource : ArgumentsProvider by arguments(" ", "\t", "\n")
+
+fun ArgumentsProvider.adapt(vararg addition: Any) = provideArguments(null)
+    .map { Arguments.of(it.get()[0], *addition) }
