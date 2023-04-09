09d2528b09d36ccb51fd25091356e2c44b6edfb0
Dmitriy Malohlovets <dmalohlovets@tzero.com>
2023-04-09 10:40:48 +0300

    refactored ArraysUtils

--- lib/src/main/kotlin/io/tzero/aqa/framework/utils/ArraysUtils.kt
+++ lib/src/main/kotlin/io/tzero/aqa/framework/utils/ArraysUtils.kt
@@ -3,12 +3,9 @@ package io.tzero.aqa.framework.utils
 import org.junit.jupiter.params.provider.Arguments
 
 object ArraysUtils {
-    fun <T, R> Array<T>.toArgumentsStream(
-        transform: (T) -> R = {
-            @Suppress("UNCHECKED_CAST")
-            it as R
-        }
-    ) = map(transform).map {
+    fun <T> Array<T>.toArgumentsStream() = toArgumentsStream { it }
+
+    fun <T, R> Array<T>.toArgumentsStream(transform: (T) -> R) = map(transform).map {
         when (it) {
             is Pair<*, *> -> Arguments.of(it.first, it.second)
             else -> Arguments.of(it)
