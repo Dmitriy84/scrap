02ed17e6331ecfa4891f9de045f842858d5506da
Dmitriy Malohlovets <dmalohlovets@tzero.com>
2023-04-09 14:07:39 +0300

    refactored sources

--- lib/src/main/kotlin/io/tzero/aqa/framework/utils/ArraysUtils.kt
+++ lib/src/main/kotlin/io/tzero/aqa/framework/utils/ArraysUtils.kt
@@ -5,7 +5,8 @@ import org.junit.jupiter.params.provider.Arguments
 object ArraysUtils {
     fun <T, R> Array<T>.toArgumentsStream(transform: (Int, T) -> R) = mapIndexed(transform).map {
         when (it) {
-            is Pair<*, *> -> Arguments.of(it.first, it.second)
+            is Pair<*, *> -> Arguments.of(it.toList())
+            is Triple<*,*,*>-> Arguments.of(it.toList())
             else -> Arguments.of(it)
         }
     }.stream()
