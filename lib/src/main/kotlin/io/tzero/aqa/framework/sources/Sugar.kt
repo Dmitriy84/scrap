4d8804fc224ead0e43635370dc3f120c7178e434
Dmitriy Malohlovets <dmalohlovets@tzero.com>
2023-04-17 13:01:44 +0300

    kotlinx-serialization-json

--- lib/src/main/kotlin/io/tzero/aqa/framework/sources/Sugar.kt
+++ lib/src/main/kotlin/io/tzero/aqa/framework/sources/Sugar.kt
@@ -3,8 +3,6 @@ package io.tzero.aqa.framework.sources
 import io.tzero.aqa.framework.utils.ArraysUtils.toArgumentsStream
 import org.junit.jupiter.params.provider.ArgumentsProvider
 
-fun <T> stream(vararg args: T) = stream({ _, it -> it }, args)
+fun <T> stream(vararg args: T) = stream({ _, it -> it }, *args)
 
-fun <T, R> stream(transform: (Int, T) -> R, vararg args: T) = ArgumentsProvider {
-    args.toArgumentsStream(transform)
-}
+fun <T, R> stream(transform: (Int, T) -> R, vararg args: T) = ArgumentsProvider { args.toArgumentsStream(transform) }
