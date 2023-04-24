c0bc9b9b2e99321099a0e91a00c54bd15eda0342
dmytromalohlovets <dmalohlovets@tzero.com>
2023-04-24 21:59:12 +0300

    +max response time check

--- lib/src/main/kotlin/io/tzero/aqa/framework/api/config/ProjectConfig.kt
+++ lib/src/main/kotlin/io/tzero/aqa/framework/api/config/ProjectConfig.kt
@@ -1,11 +1,8 @@
 package io.tzero.aqa.framework.api.config
 
 import io.restassured.RestAssured
-import io.restassured.config.HttpClientConfig
 import io.restassured.filter.log.RequestLoggingFilter
 import io.restassured.filter.log.ResponseLoggingFilter
-import org.apache.http.client.config.RequestConfig
-import org.apache.http.impl.client.HttpClientBuilder
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.*
 
@@ -25,23 +22,13 @@ open class ProjectConfig {
     open fun getRestAssured(): RestAssured {
         RestAssured.baseURI = baseURL
         RestAssured.filters(RequestLoggingFilter(), ResponseLoggingFilter())
-
-        val requestConfig = RequestConfig.custom()
-            .setConnectTimeout(timeout.toInt())
-            .setConnectionRequestTimeout(timeout.toInt())
-            .setSocketTimeout(timeout.toInt())
-            .build()
-        val httpClientFactory = HttpClientConfig.httpClientConfig()
-            .httpClientFactory {
-                HttpClientBuilder.create()
-                    .setDefaultRequestConfig(requestConfig)
-                    .build()
-            }
-        RestAssured.config = RestAssured
-            .config()
-            .httpClient(httpClientFactory)
-
         RestAssured.useRelaxedHTTPSValidation()
+        timeoutStatic = timeout.toLong()
         return RestAssured()
+    }
+
+    companion object {
+        @JvmStatic
+        var timeoutStatic: Long = 0
     }
 }
--- lib/src/main/kotlin/io/tzero/aqa/framework/api/specs/ResponseSpec.kt
+++ lib/src/main/kotlin/io/tzero/aqa/framework/api/specs/ResponseSpec.kt
@@ -1,17 +1,22 @@
 package io.tzero.aqa.framework.api.specs
 
 import io.restassured.builder.ResponseSpecBuilder
+import io.tzero.aqa.framework.api.config.ProjectConfig
+import org.hamcrest.Matchers
 
 object ResponseSpec {
     val `200` = ResponseSpecBuilder()
+        .expectResponseTime(Matchers.lessThan(ProjectConfig.timeoutStatic))
         .expectStatusCode(200)
         .build()!!
 
     val `201` = ResponseSpecBuilder()
+        .expectResponseTime(Matchers.lessThan(ProjectConfig.timeoutStatic))
         .expectStatusCode(201)
         .build()!!
 
     val `400` = ResponseSpecBuilder()
+        .expectResponseTime(Matchers.lessThan(ProjectConfig.timeoutStatic))
         .expectStatusCode(400)
         .build()!!
 }
