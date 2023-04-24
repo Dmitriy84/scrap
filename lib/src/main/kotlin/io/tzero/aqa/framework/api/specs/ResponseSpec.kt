efc0251419c79fd8746be5abc3e52eb7fb1d0799
dmytromalohlovets <dmalohlovets@tzero.com>
2023-04-24 22:58:29 +0300

    refactored spec

--- lib/src/main/kotlin/io/tzero/aqa/framework/api/specs/ResponseSpec.kt
+++ lib/src/main/kotlin/io/tzero/aqa/framework/api/specs/ResponseSpec.kt
@@ -5,18 +5,14 @@ import io.tzero.aqa.framework.api.config.ProjectConfig
 import org.hamcrest.Matchers
 
 object ResponseSpec {
-    val `200` = ResponseSpecBuilder()
-        .expectResponseTime(Matchers.lessThan(ProjectConfig.timeoutStatic))
-        .expectStatusCode(200)
-        .build()!!
+    val `200` = 200.toResponseSpec()
 
-    val `201` = ResponseSpecBuilder()
-        .expectResponseTime(Matchers.lessThan(ProjectConfig.timeoutStatic))
-        .expectStatusCode(201)
-        .build()!!
+    val `201` = 201.toResponseSpec()
 
-    val `400` = ResponseSpecBuilder()
+    val `400` = 400.toResponseSpec()
+
+    private fun Int.toResponseSpec() = ResponseSpecBuilder()
         .expectResponseTime(Matchers.lessThan(ProjectConfig.timeoutStatic))
-        .expectStatusCode(400)
+        .expectStatusCode(this)
         .build()!!
 }
