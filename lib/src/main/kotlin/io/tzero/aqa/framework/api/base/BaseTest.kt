d74c109254f8d749672f8d333d62f0da28da0788
Dmitriy Malohlovets <dmalohlovets@tzero.com>
2023-05-02 19:20:36 +0300

    getObjectFromJson

--- lib/src/main/kotlin/io/tzero/aqa/framework/api/base/BaseTest.kt
+++ lib/src/main/kotlin/io/tzero/aqa/framework/api/base/BaseTest.kt
@@ -1,10 +1,13 @@
 package io.tzero.aqa.framework.api.base
 
+import io.kotest.core.spec.style.AnnotationSpec
+import io.tzero.aqa.framework.api.config.ProjectConfig
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromStream
 import org.junit.platform.commons.logging.Logger
 import org.junit.platform.commons.logging.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
-import io.tzero.aqa.framework.api.config.ProjectConfig
-import io.kotest.core.spec.style.AnnotationSpec
 
 @SpringBootTest(classes = [ProjectConfig::class])
 open class BaseTest : AnnotationSpec() {
@@ -15,4 +18,9 @@ open class BaseTest : AnnotationSpec() {
 
     protected inline fun <reified R> getObjectFromJson(path: String) =
         json.decodeFromStream<R>(javaClass.classLoader.getResourceAsStream("data/$env/$path"))
+
+    protected val json = Json {
+        encodeDefaults = true
+        isLenient = true
+    }
 }
