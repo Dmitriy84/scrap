e632df1a35ea563b85cd352c29d43614062e37f5
Dmitriy Malohlovets <dmalohlovets@tzero.com>
2023-05-03 16:20:08 +0300

    ResponseSpec refactored

--- lib/src/main/kotlin/io/tzero/aqa/framework/api/specs/ResponseSpec.kt
+++ lib/src/main/kotlin/io/tzero/aqa/framework/api/specs/ResponseSpec.kt
@@ -3,9 +3,12 @@ package io.tzero.aqa.framework.api.specs
 import io.restassured.builder.ResponseSpecBuilder
 
 object ResponseSpec {
-    val `200` = 200.toResponseSpec()
-    val `201` = 201.toResponseSpec()
-    val `400` = 400.toResponseSpec()
+    val `200`
+        get() = 200.toResponseSpec()
+    val `201`
+        get() = 201.toResponseSpec()
+    val `400`
+        get() = 400.toResponseSpec()
 
     private fun Int.toResponseSpec() = ResponseSpecBuilder()
         .expectStatusCode(this)
