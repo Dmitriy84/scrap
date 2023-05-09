67492277f56a21ada02868e05b14e1b4e8cb0e77
Dmitriy Malohlovets <dmalohlovets@tzero.com>
2023-05-10 01:12:23 +0300

    public json

--- lib/src/main/kotlin/io/tzero/aqa/framework/utils/StringUtils.kt
+++ lib/src/main/kotlin/io/tzero/aqa/framework/utils/StringUtils.kt
@@ -1,5 +1,6 @@
 package io.tzero.aqa.framework.utils
 
+import io.tzero.aqa.framework.api.support.IJson
 import org.apache.commons.lang3.RandomStringUtils
 
 object StringUtils {
@@ -7,4 +8,10 @@ object StringUtils {
         RandomStringUtils.randomAlphanumeric(numberOfChars) + "@tzero.com"
 
     fun Any.wrap() = toString().let { "\"$it\"" }
+
+    fun Any.toBody() =
+        when (this) {
+            is IJson -> toJson()
+            else -> toString().trimIndent()
+        }
 }
