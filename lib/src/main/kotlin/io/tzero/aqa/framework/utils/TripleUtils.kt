eca97fee4c9172d9534d83d2394e8bc67c67715d
Dmitriy Malohlovets <dmalohlovets@tzero.com>
2023-05-19 17:45:14 +0300

    +TripleUtils

--- lib/src/main/kotlin/io/tzero/aqa/framework/utils/TripleUtils.kt
+++ lib/src/main/kotlin/io/tzero/aqa/framework/utils/TripleUtils.kt
@@ -1,6 +1,6 @@
 package io.tzero.aqa.framework.utils
 
-class TripleUtils {
+object TripleUtils {
     infix fun <T, U, S, V> ((T, U, S) -> V).callWith(arguments: Triple<T, U, S>): V =
         this(arguments.first, arguments.second, arguments.third)
 }
