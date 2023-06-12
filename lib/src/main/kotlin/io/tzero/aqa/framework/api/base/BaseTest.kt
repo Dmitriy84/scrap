7d45a40ad20bc0bd09e7b68b6e5d760de49d5e12
Dmitriy Malohlovets <dmalohlovets@tzero.com>
2023-06-12 18:32:00 +0300

    +resultOf

--- lib/src/main/kotlin/io/tzero/aqa/framework/api/base/BaseTest.kt
+++ lib/src/main/kotlin/io/tzero/aqa/framework/api/base/BaseTest.kt
@@ -33,4 +33,14 @@ open class BaseTest : AnnotationSpec() {
             ignoreUnknownKeys = true
         }
     }
+
+    protected inline fun <T, R> T.resultOf(block: T.() -> R): Result<R> {
+        return try {
+            Result.success(block())
+        } catch (e: CancellationException) {
+            throw e
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
 }
