f550f95958e8e208b669f676ca1a7b3f659608ca
Dmitriy Malohlovets <dmalohlovets@tzero.com>
2023-09-04 20:14:08 +0300

    refactored

--- web/src/test/kotlin/dmalohlovets/djinni/DjinniConfig.kt
+++ web/src/test/kotlin/dmalohlovets/djinni/DjinniConfig.kt
@@ -1,6 +1,6 @@
 package dmalohlovets.djinni
 
-import io.tzero.aqa.framework.api.config.YamlPropertySource
+import aqa.framework.api.config.YamlPropertySource
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
