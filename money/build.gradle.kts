9e0b2e2507446ff997edbd5ed9b43ae9766f6fc7
Dmitriy Malohlovets <dmalohlovets@tzero.com>
2023-11-28 18:32:01 +0200

    "CIRCLE_WORKFLOW_ID" to System.getenv("CIRCLE_WORKFLOW_ID"),

--- money/build.gradle.kts
+++ money/build.gradle.kts
@@ -3,4 +3,6 @@ dependencies{
     testImplementation(libs.selenium)
     testImplementation(project(mapOf("path" to ":web")))
     testImplementation(libs.dynamodb)
+    testImplementation(platform("software.amazon.awssdk:bom:2.21.27"))
+    testImplementation("software.amazon.awssdk:dynamodb-enhanced")
 }
