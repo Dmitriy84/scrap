e12d6866928428f270c411614231988f40b43a4d
Dmitriy84 <dmytro.malohlovets@gmail.com>
2026-05-11 17:16:17 +0300

    + lcd

---
+++ arduino/lcd/lcd.ino
@@ -0,0 +1,19 @@
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+
+    lcd.setCursor(3, 0);
+    lcd.print("Hello, World!");
+}
+
+void loop() {
+    lcd.setCursor(0, 1);
+    lcd.print("Seconds: ");
+    lcd.print(millis() / 1000);
+    delay(1000);
+  // Nothing to do in the loop
+}
