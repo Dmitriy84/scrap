fb48270b8fe36dd9deb9656cccbe541a6bb4818a
Dmitriy84 <dmytro.malohlovets@gmail.com>
2026-05-11 17:27:17 +0300

    + lcd

--- arduino/lcd/lcd.ino
+++ arduino/lcd/lcd.ino
@@ -5,15 +5,14 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 void setup() {
   lcd.init();
   lcd.backlight();
-
-    lcd.setCursor(3, 0);
-    lcd.print("Hello, Makar!");
+  lcd.setCursor(3, 0);
+  lcd.print("Hello, Makar!");
 }
 
 void loop() {
-    lcd.setCursor(0, 1);
-    lcd.print("Seconds: ");
-    lcd.print(millis() / 1000);
-    delay(1000);
+  lcd.setCursor(0, 1);
+  lcd.print("Seconds: ");
+  lcd.print(millis() / 1000);
+  delay(1000);
   // Nothing to do in the loop
 }
